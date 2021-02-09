ea6c9364f5c2d6cdc0028fcfe7ea16b2cb74f675
Alex-INP <69797796+Alex-INP@users.noreply.github.com>
2021-02-09 17:48:25 +0300

    Delete Lesson_1_Task_1.py

--- Lesson_1_Task_1.py
+++
@@ -1,8 +0,0 @@
-duration = int(input("Укажите суммарное количество секунд: "))
-
-days = duration // 86400
-hours = (duration % 86400) // 3600
-minutes = (duration % 3600) // 60
-seconds = duration % 60
-
-print(days, "дней", hours, "часов", minutes, "минут", seconds, "секунд")
