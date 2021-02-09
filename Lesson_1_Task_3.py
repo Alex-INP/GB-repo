0296d5ad2245d5346c86fc6e626bed2cd2db595c
Alex-INP <69797796+Alex-INP@users.noreply.github.com>
2021-02-09 17:48:41 +0300

    Delete Lesson_1_Task_3.py

--- Lesson_1_Task_3.py
+++
@@ -1,8 +0,0 @@
-user_num = int(input("Введите число от 1 до 20: "))
-
-if user_num == 1:
-	print(user_num,"процент")
-elif 1 < user_num < 5:
-	print(user_num,"процента")
-else:
-	print(user_num, "процентов")
